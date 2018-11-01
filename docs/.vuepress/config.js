64cf44a38a33e57a58df011d10c6136bf85f748c
pashtunzoy <asifkhannoorzai@gmail.com>
2018-11-02 08:44:27 +1100

    Added a how to add a new folder

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,18 +2,19 @@ const dirTree = require('directory-tree');
 const fs = require('fs');
 const path = require('path');
 
-let mdFiles = dirTree('docs/', {normalizePath: true}).children;
-mdFiles.splice(-1);
-mdFiles = mdFiles.slice(1).map(d => {
-
-	return {
-		title: d.path.split('_').slice(1).join(' '),
-		collapsable: false,
-		children: d.children.map(ch => {
-			if (ch.name === 'README.md') return `${ch.path.split('/')[1]}/`;
-			else return ch.path.split('/').slice(1).join('/').split('.')[0];
-		})
-	}
+let DirStructure = dirTree('docs/', {normalizePath: true}).children;
+let mdFiles = [];
+DirStructure.forEach(d => {
+  if (d.name !== ".vuepress" && d.name !== "README.md") {
+    mdFiles.push({
+      title: d.path.split('/').slice(1).join(' ').split("_").join(" "),
+      collapsable: false,
+      children: d.children.map(ch => {
+        if (ch.name === 'README.md') return `${ch.path.split('/')[1]}/`;
+        else return ch.path.split('/').slice(1).join('/').split('.')[0];
+      })
+    })
+  }
 });
 
 module.exports = {
---
+++ docs/example_folder/README.md
@@ -0,0 +1,11 @@
+---
+title: New Directory
+---
+
+## Create new folder
+
+In order to create a new folder, you basically create a new folder inside the `docs` directory.
+When creating a new folder, you have to add a `README.md` into the folder as it's front page for the user when they visit the site using a specific page url.
+The below screenshot will give you a better understanding of how creating new folders seems like a breeze.
+
+![EXAMPLE_FOLDER](/images/EXAMPLE_FOLDER.png)
