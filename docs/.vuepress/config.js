b0f185d5799eb37de24792dc337cebe91fd08398
Pashtunzoy <asifkhannoorzai@gmail.com>
2018-11-14 12:26:43 +1100

    Testing out netlify cms

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,29 +4,29 @@ const path = require('path');
 
 let DirStructure = dirTree('docs/', {normalizePath: true}).children;
 let mdFiles = [];
-
+mdFiles.push('/');
 DirStructure.forEach(d => {
   if (d.name !== ".vuepress" && d.name !== "README.md") {
-    mdFiles.push({
-      title: d.path.split('/').slice(1).join(' ').split("_")[1],
-      collapsable: false,
-      children: d.children.map(ch => {
-        if (ch.name === 'README.md') return `${ch.path.split('/')[1]}/`;
-        else return ch.path.split('/').slice(1).join('/').split('.')[0];
-      })
-    })
+		d.name = d.name.split('.md')[0];
+		mdFiles.push(`/${d.name}`);
   }
 });
+
+console.log(mdFiles)
 
 module.exports = {
   title: 'Nauticus Help Center',
   description: 'FAQ and other Helpful Information',
-  base: "/help/",
+  base: "/",
   themeConfig: {
     docsDir: 'docs',
     repo: 'NauticusOfficial/help-center',
 		sidebar: mdFiles,
     nav: [
+			{
+				text: 'Admin',
+				link: '/admin/#/',
+			}
     ]
   }
 }
