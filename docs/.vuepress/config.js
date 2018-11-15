1c967a6b33403192ab48648df530088e912c65e7
Pashtunzoy <asifkhannoorzai@gmail.com>
2018-11-15 12:02:49 +1100

    Set the cleanUrl option to true

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
   title: 'Nauticus Help Center',
   description: 'FAQ and other Helpful Information',
 	base: "/",
-	cleanURL: false,
+	cleanURL: true,
   themeConfig: {
     docsDir: 'docs',
 		repo: 'NauticusOfficial/help-center',
